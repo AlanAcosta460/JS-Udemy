84f45f74597d144d2c3d30328078c1ef4a1bac1b
Alan Acosta <alanomar460@gmail.com>
2023-04-05 12:17:28 -0600

    introducción a JS

---
+++ 01-Introduccion/JS/app.js
@@ -0,0 +1,26 @@
+// ** Primer Hola Mundo en JavaScript **
+// alert('Hola Mundo');
+
+// ** Registrar el nombre y cambiar el HTML para mostraarlo en pantalla **
+// const nombre = prompt('Ingrese su Nombre');
+// document.querySelector('.contenido').innerHTML = `${nombre}`;
+
+// ** Mensajes en la consola de JS en el navegador **
+// console.log([1, 2, 3, 4]); // Imrpime el arreglo
+// console.error('Algo salió mal'); // Muesstra un mensaje de error
+// console.warn('Algo salió mal'); // Muestra una advertencia
+
+// ** Manejo del código **
+/* 
+    Sintaxis Válida
+console.log('...')
+console.log('....') // Una instrucción por linea
+
+console.log('...');
+console.log('....'); // No es necesario el punto y coma
+
+console.log('...'); console.log('....'); // No recomendado
+
+    Sintaxis Inválida
+console.log('...') console.log('....')
+*/
